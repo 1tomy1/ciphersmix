f105c30512b35ac272517deedc8dbca7c6807e81
1tomy1 <tomi1995.jerenko@gmail.com>
2018-11-02 11:10:20 +0100

    Hill cipher implementation

---
+++ ciphersmix/wwwroot/js/hillcipher.js
@@ -0,0 +1,198 @@
+﻿$(document).ready(() => {
+    $('#supportedCharacters').html(Object.keys(dict));
+});
+
+var dict = {
+    "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9,
+    "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18,
+    "t": 19, "u": 20, "v": 21, "x": 22, "w": 23, "y": 24, "z": 25, ".": 26, "!": 27,
+    ",": 28, "?": 29, " ": 30, "1": 31, "2": 32, "3": 33, "4": 34, "5": 35, "6": 36,
+    "7": 37, "8": 38, "9": 39, "0": 40, ":": 41, ";": 42
+};
+
+//generates id where its matrix determinant has GCD 1 (for that reason prime number is chosen as modulus base),
+//key matrix determinant != 0
+function makeid(keysize) {
+    let text = "";
+    let possible = Object.keys(dict).join('');
+    for (let i = 0; i < keysize; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    if (math.det(stringArrayToMatrix(chunkString(text, math.sqrt(keysize)))) === 0)
+        return makeid(keysize);
+    return text;
+}
+
+function generateKey() {
+    $('#keycontents').val(makeid(math.pow($('#matrixBlockSizeInput').val(), 2)));
+}
+
+//divides string into chunks/blocks
+function chunkString(str, len) {
+    var _size = Math.ceil(str.length / len),
+        _ret = new Array(_size),
+        _offset;
+
+    for (var _i = 0; _i < _size; _i++) {
+        _offset = _i * len;
+        _ret[_i] = str.substring(_offset, _offset + len);
+    }
+    _ret[_ret.length - 1] = _ret[_ret.length - 1].padEnd(len, " ");
+    return _ret;
+}
+
+//creates matrix out of string chunks by mapping characters to corresponding number values
+function stringArrayToMatrix(chunkedStringArray) {
+    var matrix = new Array(chunkedStringArray.length);
+    for (var i = 0; i < chunkedStringArray.length; i++) {
+        var arrayItem = chunkedStringArray[i];
+        var chunkArray = new Array(arrayItem.length);
+        for (var j = 0; j < arrayItem.length; j++) {
+            chunkArray[j] = dict[arrayItem[j].toLowerCase()];
+        }
+        matrix[i] = chunkArray;
+    }
+    return matrix;
+}
+
+//encrypts the message by multiplying key matrix and message blocks
+//performs mod operation on results
+function encode() {
+    var plaintext = $('#contentToEncode').val();
+    var key = $('#keycontents').val();
+
+    var keyMatrix = stringArrayToMatrix(chunkString(key, math.sqrt(key.length)));
+    var plainTextMatrix = stringArrayToMatrix(chunkString(plaintext, math.sqrt(key.length)));
+
+    var cypherText = new Array(plainTextMatrix.length);
+    var modBase = Object.keys(dict).length;
+
+    for (var i = 0; i < plainTextMatrix.length; i++) {
+        var encodedBlock = math.multiply(keyMatrix, plainTextMatrix[i]);
+        for (var j = 0; j < encodedBlock.length; j++) {
+            for (var property in dict) {
+                if (dict[property] === encodedBlock[j] % modBase) {
+                    encodedBlock[j] = property;
+                    break;
+                }
+            }
+        }
+        cypherText[i] = encodedBlock;
+    }
+    $('#contentToDecode').val(cypherText.flat().join(''));
+}
+
+function decode() {
+    var cypherText = $('#contentToDecode').val();
+    var key = $('#keycontents').val();
+    var keyMatrix = stringArrayToMatrix(chunkString(key, math.sqrt(key.length)));
+    var cypherTextMatrix = stringArrayToMatrix(chunkString(cypherText, math.sqrt(key.length)));
+
+    //step 1: modular multiplicative inverse of key matrix determinant mod base
+    var modBase = Object.keys(dict).length;
+    var keyMatrixDeterminant = math.mod(math.ceil(math.det(keyMatrix)), Object.keys(dict).length);
+    var keyDetModInverse = modInverse(keyMatrixDeterminant, modBase);
+
+    //step 2: find matrix of cofactors
+    let cofactorMatrix = lightmatrix.cofactors(keyMatrix);
+
+    //step 3: key matrix transposition
+    let transposed = math.transpose(cofactorMatrix);
+
+    //step 5: key matrix multiplication by mod inverse and mod operation
+    let multiplied = math.multiply(keyDetModInverse, transposed);
+
+    //step 6: get mod value of each matrix cell
+    let invMatrix = math.mod(multiplied, modBase);
+
+    //step 7: Calculate plaintext
+    var plaintext = new Array(cypherTextMatrix.length);
+    for (var i = 0; i < cypherTextMatrix.length; i++) {
+        var decodedBlock = math.multiply(invMatrix, cypherTextMatrix[i]);
+        for (var j = 0; j < decodedBlock.length; j++) {
+            for (let property in dict) {
+                if (dict[property] === decodedBlock[j] % modBase) {
+                    decodedBlock[j] = property;
+                    break;
+                }
+            }
+        }
+        plaintext[i] = decodedBlock;
+    }
+    $('#contentToEncode').val(plaintext.flat().join(''));
+}
+
+//Finds inverse of a mod m
+//Source: Dipu - https://stackoverflow.com/questions/26985808/calculating-the-modular-inverse-in-javascript
+function modInverse(a, m) {
+    // validate inputs
+    [a, m] = [Number(a), Number(m)];
+    if (Number.isNaN(a) || Number.isNaN(m)) {
+        return NaN; // invalid input
+    }
+    a = (a % m + m) % m;
+    if (!a || m < 2) {
+        return NaN // invalid input
+    }
+    // find the gcd
+    const s = [];
+    let b = m;
+    while (b) {
+        [a, b] = [b, a % b];
+        s.push({ a, b });
+    }
+    if (a !== 1) {
+        return NaN; // inverse does not exists
+    }
+    // find the inverse
+    let x = 1;
+    let y = 0;
+    for (let i = s.length - 2; i >= 0; --i) {
+        [x, y] = [y, x - y * Math.floor(s[i].a / s[i].b)];
+    }
+    return (y % m + m) % m;
+}
+
+//open file logic
+//window.onload = function () {
+//    if (window.File && window.FileReader && window.FileList && window.Blob) {
+//        var plainText = document.getElementById('fileToEncode');
+//        plainText.addEventListener('change', function (e) {
+//            var fileExtension = /text.*/;
+//            var fileTobeRead = plainText.files[0];
+//            if (fileTobeRead.type.match(fileExtension)) {
+//                var fileReader = new FileReader();
+//                fileReader.onload = function (e) {
+//                    var fileContents = document.getElementById('contentToEncode');
+//                    fileContents.innerText = fileReader.result;
+//                };
+//                fileReader.readAsText(fileTobeRead);
+//            }
+//            else {
+//                alert("Please select text file");
+//            }
+
+//        }, false);
+
+//        var encodedText = document.getElementById('fileToDecode');
+//        encodedText.addEventListener('change', function (e) {
+//            var fileExtension = /text.*/;
+//            var fileTobeRead = encodedText.files[0];
+//            if (fileTobeRead.type.match(fileExtension)) {
+//                var fileReader = new FileReader();
+//                fileReader.onload = function (e) {
+//                    var fileContents = document.getElementById('contentToDecode');
+//                    fileContents.innerText = fileReader.result;
+//                };
+//                fileReader.readAsText(fileTobeRead);
+//            }
+//            else {
+//                alert("Please select text file");
+//            }
+
+//        }, false);
+//    }
+//    else {
+//        alert("Files are not supported");
+//    }
+//};
---
+++ ciphersmix/wwwroot/lib/lightmatrix/lightMatrix.js
@@ -0,0 +1,290 @@
+﻿//source: https://www.npmjs.com/package/lightmatrix
+
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (global.lightmatrix = factory());
+}(this, (function () {
+    'use strict';
+
+    function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+
+    var isNumber = function isNumber(num) {
+        return typeof num === 'number' && !isNaN(num);
+    };
+
+    var isNumeric = function isNumeric(num) {
+        var number = Number(String(num instanceof Object ? true : num));
+        return isNumber(number);
+    };
+
+    var reciprocal = function reciprocal(num) {
+        return 1 / num;
+    };
+
+    var normalizeNumber = function normalizeNumber(num) {
+        return num === 0 ? 0 : num;
+    };
+
+    var isMatrix = function isMatrix(matrix) {
+
+        if (!(matrix instanceof Array) || matrix.length === 0) return false;
+
+        var cols = void 0;
+
+        var _matrix = _toArray(matrix),
+            cloneMatrix = _matrix.slice(0);
+
+        while (cloneMatrix.length > 0) {
+            var row = cloneMatrix.shift();
+            var isArrayRow = row instanceof Array;
+
+            if (!isArrayRow) return false;
+
+            cols = cols || row.length;
+
+            var sameRowCols = row.length === cols;
+            var numericRowElements = row.filter(function (e) {
+                return isNumeric(e);
+            }).length === cols;
+
+            if (!(sameRowCols && numericRowElements)) return false;
+        }
+
+        return true;
+    };
+
+    var pivotMatrix = function pivotMatrix(matrix, row, col) {
+        var newMatrix = [];
+
+        for (var i = 0; i < matrix.length; i += 1) {
+            if (i !== row) {
+                var newRow = [];
+                for (var j = 0; j < matrix[0].length; j += 1) {
+                    if (j !== col) {
+                        newRow.push(normalizeNumber(matrix[i][j]));
+                    }
+                }
+                newMatrix.push(newRow);
+            }
+        }
+
+        return newMatrix;
+    };
+
+    var scalarProduct = function scalarProduct(n, matrix) {
+        var _matrix2 = _toArray(matrix),
+            newMatrix = _matrix2.slice(0);
+
+        var dim = [matrix.length, matrix[0].length];
+
+        for (var i = 0; i < dim[0]; i += 1) {
+            for (var j = 0; j < dim[1]; j += 1) {
+                newMatrix[i][j] = normalizeNumber(matrix[i][j] * n);
+            }
+        }
+
+        return newMatrix;
+    };
+
+    var matrixProduct = function matrixProduct(matA, matB) {
+
+        var dimA = [matA.length, matA[0].length];
+        var dimB = [matB.length, matB[0].length];
+        var matrix = [];
+
+        if (dimA[1] === dimB[0]) {
+            for (var i = 0; i < dimA[0]; i += 1) {
+                matrix[i] = [];
+                for (var j = 0; j < dimB[1]; j += 1) {
+                    var s = 0;
+
+                    for (var k = 0; k < dimA[1]; k += 1) {
+                        s += matA[i][k] * matB[k][j];
+                    }
+
+                    matrix[i][j] = normalizeNumber(s);
+                }
+            }
+            return matrix;
+        }
+
+        throw new Error('Matrices not compatible.');
+    };
+
+    var sum = function sum(a, b) {
+
+        if (!(isMatrix(a) && isMatrix(b))) {
+            throw new Error('Invalid matrix specification.');
+        }
+
+        var matrix = [];
+        var dimA = [a.length, a[0].length];
+        var dimB = [b.length, b[0].length];
+
+        if (!(dimA[0] === dimB[0] && dimA[1] === dimB[1])) {
+            throw new Error('Matrices not compatible.');
+        }
+
+        for (var i = 0; i < dimA[0]; i += 1) {
+            matrix[i] = [];
+            for (var j = 0; j < dimA[1]; j += 1) {
+                matrix[i][j] = normalizeNumber(a[i][j] + b[i][j]);
+            }
+        }
+
+        return matrix;
+    };
+
+    var product = function product(a, b) {
+
+        if (isNumber(a)) {
+            if (isNumber(b)) return a * b; else if (isMatrix(b)) return scalarProduct(a, b);
+        } else if (isMatrix(a)) {
+            if (isNumber(b)) return scalarProduct(b, a); else if (isMatrix(b)) return matrixProduct(a, b);
+        }
+
+        throw new Error('Invalid arguments supplied.');
+    };
+
+    var Matrix = {
+
+        ok: isMatrix,
+
+        dimension: function dimension(matrix) {
+            if (this.ok(matrix)) {
+                return [matrix.length, matrix[0].length];
+            }
+            throw new Error('Invalid matrix specification.');
+        },
+        unit: function unit(dimension) {
+            var dim = dimension >= 1 && dimension || 2;
+            var unitmatrix = [];
+
+            for (var i = 0; i < dim; i += 1) {
+                unitmatrix[i] = [];
+                for (var j = 0; j < dim; j += 1) {
+                    unitmatrix[i][j] = i === j ? 1 : 0;
+                }
+            }
+
+            return unitmatrix;
+        },
+        equal: function equal(a, b) {
+            var dimA = this.dimension(a);
+            var dimB = this.dimension(b);
+
+            if (!(dimA[0] === dimB[0] && dimA[1] === dimB[1])) return false;
+
+            for (var i = 0; i < dimA[0]; i += 1) {
+                for (var j = 0; j < dimA[1]; j += 1) {
+                    if (a[i][j] !== b[i][j]) return false;
+                }
+            }
+
+            return true;
+        },
+
+
+        sum: sum,
+
+        product: product,
+
+        transpose: function transpose(matrix) {
+            var dim = this.dimension(matrix).reverse();
+            var newmatrix = [];
+
+            for (var i = 0; i < dim[0]; i += 1) {
+                newmatrix[i] = [];
+                for (var j = 0; j < dim[1]; j += 1) {
+                    newmatrix[i][j] = normalizeNumber(matrix[j][i]);
+                }
+            }
+
+            return newmatrix;
+        },
+        determinant: function determinant(matrix) {
+
+            var det = void 0;
+            var dim = this.dimension(matrix);
+
+            if (dim[0] !== dim[1]) throw new Error('Square matrix required.');
+
+            if (dim[0] === 1) return matrix[0][0];
+
+            for (var i = 0; i < matrix[0].length; i += 1) {
+                det = det || 0;
+                det += ((i & 1) === 0 ? 1 : -1) * matrix[0][i] * this.determinant(pivotMatrix(matrix, 0, i));
+            }
+
+            return det;
+        },
+        minors: function minors(matrix) {
+
+            var dim = this.dimension(matrix);
+            var newmatrix = [];
+
+            if (dim[0] !== dim[1]) throw new Error('Square matrix required.');
+
+            for (var i = 0; i < dim[0]; i += 1) {
+                newmatrix[i] = [];
+                for (var j = 0; j < dim[0]; j += 1) {
+                    newmatrix[i][j] = normalizeNumber(this.determinant(pivotMatrix(matrix, i, j)));
+                }
+            }
+
+            return newmatrix;
+        },
+        cofactors: function cofactors(matrix) {
+            var dim = this.dimension(matrix);
+            var newmatrix = this.minors(matrix);
+
+            for (var i = 0; i < dim[0]; i += 1) {
+                for (var j = 0; j < dim[0]; j += 1) {
+                    newmatrix[i][j] = normalizeNumber(newmatrix[i][j] * ((i + j & 1) === 0 ? 1 : -1));
+                }
+            }
+
+            return newmatrix;
+        },
+        adjoint: function adjoint(matrix) {
+            return this.transpose(this.cofactors(matrix));
+        },
+        inverse: function inverse(matrix) {
+            var det = this.determinant(matrix);
+
+            if (det === 0) throw new Error('Cannot compute inverse of singular matrix.');
+
+            return this.product(reciprocal(det), this.adjoint(matrix));
+        }
+    };
+
+    var bindMatrix = function bindMatrix(method) {
+        return Matrix[method].bind(Matrix);
+    };
+
+    var lightmatrix = {
+        ok: bindMatrix('ok'),
+        valid: bindMatrix('ok'),
+        order: bindMatrix('dimension'),
+        dimension: bindMatrix('dimension'),
+        unit: bindMatrix('unit'),
+        identity: bindMatrix('unit'),
+        equal: bindMatrix('equal'),
+        same: bindMatrix('equal'),
+        sum: bindMatrix('sum'),
+        add: bindMatrix('sum'),
+        product: bindMatrix('product'),
+        multiply: bindMatrix('product'),
+        transpose: bindMatrix('transpose'),
+        determinant: bindMatrix('determinant'),
+        minors: bindMatrix('minors'),
+        cofactors: bindMatrix('cofactors'),
+        adjoint: bindMatrix('adjoint'),
+        inverse: bindMatrix('inverse')
+    };
+
+    return lightmatrix;
+
+})));
+//# sourceMappingURL=lightmatrix.js.map
